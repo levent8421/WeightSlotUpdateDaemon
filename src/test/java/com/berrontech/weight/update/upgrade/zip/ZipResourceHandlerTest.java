e40fa0891fe7e632a8a7ac52d043aa7c5b767844
levent8421 <levent8421@outlook.com>
2020-11-25 21:06:07 +0800

    多环境配置

--- src/test/java/com/berrontech/weight/update/upgrade/zip/ZipResourceHandlerTest.java
+++ src/test/java/com/berrontech/weight/update/upgrade/zip/ZipResourceHandlerTest.java
@@ -1,5 +1,6 @@
 package com.berrontech.weight.update.upgrade.zip;
 
+import com.berrontech.weight.update.upgrade.ExtractListener;
 import com.berrontech.weight.update.upgrade.UpgradeException;
 import com.berrontech.weight.update.upgrade.UpgradeResource;
 import org.testng.annotations.Test;
@@ -14,7 +15,28 @@ public class ZipResourceHandlerTest {
     public void testExtractFiles() throws UpgradeException {
         final UpgradeResource resource = new UpgradeResource(new File("D:\\monolith\\星巴克重力货道\\server\\upgrade-0.1.25\\upgrade-0.1.25.zip"));
         final File target = new File("D:\\monolith\\星巴克重力货道\\server\\upgrade-0.1.25\\target");
-        final List<String> res = zipResourceHandler.extractFiles(resource, target);
+        final List<String> res = zipResourceHandler.extractFiles(resource, target, new ExtractListener() {
+            @Override
+            public void onStart() {
+                System.out.println("Start");
+            }
+
+            @Override
+            public void onExtract(String filename) {
+                System.out.println("Extra:" + filename);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Complete");
+            }
+
+            @Override
+            public void onError(Throwable err) {
+                System.out.println("Error");
+                err.printStackTrace();
+            }
+        });
         System.out.println(res);
     }
 }
